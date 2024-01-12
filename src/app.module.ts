a0a84e329d2fec1066251cc82e5d193062b5cc22
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-12 14:09:10 -0300

    Terminando configuracoes iniciais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
-import { Module } from '@nestjs/common';
-import { UsuariosModule } from './usuarios/usuarios.module';
+import { Module } from '@nestjs/common'
+import { UsuariosModule } from './usuarios/usuarios.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DbConfigService } from './config/db.config.service'
+import { ConfigModule } from '@nestjs/config'
 @Module({
-  imports: [UsuariosModule],
+  imports: [
+    UsuariosModule,
+    TypeOrmModule.forRootAsync({
+      useClass: DbConfigService,
+      inject: [DbConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
